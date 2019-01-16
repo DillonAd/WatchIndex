fdd8d48b61431c389e6abf0f9b0975b773954d7a
dillon <28228453+DillonAd@users.noreply.github.com>
2019-01-15 20:00:27 -0600

    All the way to Hulu search page works

--- HuluAggregator.cs
+++ HuluAggregator.cs
@@ -38,14 +38,46 @@ namespace WatchIndex
             while(submitBtn != null);
 
             var profiles = _webDriver.FindElements(By.ClassName("Nav__label"));
-            var profile = profiles.FirstOrDefault(p => p.Text == profileName);
 
+            var profile = profiles.FirstOrDefault(p => p.Text == profileName);
             profile.Click();
+            
+            Thread.Sleep(5000);
         }
 
         public override IEnumerable<string> GetListings()
         {
-            return new string[0];
+            const string listingUri = "https://www.hulu.com/search";
+
+            var listings = new HashSet<string>();
+
+            _webDriver.Url = listingUri;
+
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                var lastPageLength = 0;
+
+                do
+                {
+                    lastPageLength = _webDriver.PageSource.Length;
+
+                    IJavaScriptExecutor js = (IJavaScriptExecutor)_webDriver;
+                    js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+                    System.Threading.Thread.Sleep(1000);
+
+                }
+                while(lastPageLength < _webDriver.PageSource.Length);
+
+                var elements = _webDriver.FindElements(By.ClassName("fallback-text"))
+                                         .Select(ele => ele.Text);
+
+                foreach (var element in elements)
+                {
+                    listings.Add(element);
+                }
+            }
+
+            return listings;
         }
 
         private IWebElement GetLogInButton() =>
